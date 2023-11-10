2762a0af68a6488e841a678ea0c24b3b92bfe944
parksb <parkgds@gmail.com>
2023-11-11 00:49:56 +0900

    Encode and decode ZapText

--- src/main/kotlin/resources/ZapText.kt
+++ src/main/kotlin/resources/ZapText.kt
@@ -4,11 +4,14 @@ import com.zap_lib.core.models.DeZapable
 import com.zap_lib.core.models.ZapPayload
 import com.zap_lib.core.models.Zapable
 import java.nio.charset.Charset
+import java.net.URLDecoder
+import java.net.URLEncoder
 
 class ZapText(val str: String, val charset: Charset = Charsets.UTF_8) : Zapable {
     override val resource: ZapResource = ZapResource.TEXT
 
-    override fun toPayload(): ZapPayload = "$str,$charset"
+    override fun toPayload(): ZapPayload =
+        "${URLEncoder.encode(str, charset.toString())},$charset"
 
     operator fun component1(): String = str
     operator fun component2(): Charset = charset
@@ -16,7 +19,7 @@ class ZapText(val str: String, val charset: Charset = Charsets.UTF_8) : Zapable 
     companion object : DeZapable {
         override fun fromPayload(payload: ZapPayload): ZapText {
             val (str, charset) = payload.split(",")
-            return ZapText(str, charset(charset))
+            return ZapText(URLDecoder.decode(str, charset), charset(charset))
         }
     }
 }
